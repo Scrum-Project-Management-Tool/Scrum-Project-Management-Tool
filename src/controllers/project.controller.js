29416ba57ee341a97a5023e2e967d9adc7a6c7b2
PranavBarthwal <pbarthwal90@gmail.com>
2024-07-22 14:32:24 +0530

    added protection to secured routes and controller for assigning projects to user

--- src/controllers/project.controller.js
+++ src/controllers/project.controller.js
@@ -2,6 +2,7 @@ import {asyncHandler} from '../utils/asyncHandler.js';
 import{ApiError} from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js"
 import { Project } from "../models/project.model.js"
+import { User } from "../models/user.model.js"
 
 
 const createNewProject = asyncHandler(async (req, res) => {
@@ -37,14 +38,44 @@ const createNewProject = asyncHandler(async (req, res) => {
 });
 
 
-const assignProject = asyncHandler(async (req, res) => {
+const assignProjectToUsers = asyncHandler(async (req, res) => {
     
-});
+    const { projectId, userIds } = req.body;
 
+    try {
+        // Check if project exists
+        const project = await Project.findById(projectId);
+        if (!project) {
+            throw new ApiError(404, "Project not found");
+        }
+
+        // Update users' projectId field
+        const users = await User.updateMany(
+            { _id: { $in: userIds } },
+            { $addToSet: { projectId: projectId } },
+            { new: true }
+        );
+
+        if (!users) {
+            throw new ApiError(500, "Error assigning project to users");
+        }
+
+        // Update project's assignees field
+        project.assignees = [...new Set([...project.assignees, ...userIds])]; // Ensure no duplicates
+        await project.save();
+
+        // Return response
+        return res.status(200).json(
+            new ApiResponse(200, "Project assigned to users successfully"));
+
+    } catch (error) {
+        throw new ApiError(500, "Error assigning project to users: " + error.message);
+    }
+});
 
 const getAllAssignedProjects = asyncHandler(async (req, res) => {
     
 });
 
 
-export {createNewProject, assignProject, getAllAssignedProjects}
+export {createNewProject, assignProjectToUsers, getAllAssignedProjects}
